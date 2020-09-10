465cb9c18ecf8c2f0ee0a88d4beff1abc700c567
crackmax <maximepichet@gmail.com>
2020-09-10 22:09:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 def majuscule(mot):
     resultat = ''
     for lettre in mot:
-       letter = ord(lettee) - 32        
+       lettre = ord(lettre) - 32        
         resultat += chr(lettre)
     return resultat
 
